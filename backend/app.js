240ffcc97fdd06d0b860d77a82fa8cc0adf0e9e6
Julien Cros <jlien7@gmail.com>
2023-04-23 10:23:47 +0200

    Addauth&middleware

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
 const dotenv = require('dotenv');
-const userRoutes = require ('/routes/user')
+const userRoutes = require ('./routes/user');
 
 dotenv.config();
 
